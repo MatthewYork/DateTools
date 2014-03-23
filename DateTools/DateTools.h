08148b0c457718677dd4bbeda020496ffb98352e
Matt York <myork@cs.ua.edu>
2014-03-23 00:55:20 -0500

    - Added DateTools.h to link entire toolset

---
+++ DateTools/DateTools.h
@@ -0,0 +1,15 @@
+//
+//  DateTools.h
+//  DateToolsExample
+//
+//  Created by Matthew York on 3/23/14.
+//
+//
+
+#import "DTConstants.h"
+#import "DTError.h"
+#import "NSDate+DateTools.h"
+#import "DTTimePeriod.h"
+#import "DTTimePeriodGroup.h"
+#import "DTTimePeriodCollection.h"
+#import "DTTimePeriodChain.h"
