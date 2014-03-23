d3225a4f98709468e47742a426dabfe188f0fcc0
Matt York <myork@cs.ua.edu>
2014-03-22 19:11:59 -0500

    - Moved testing classes out of example project and into testing project

---
+++ Tests/DateToolsTests/DateToolsTests/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DateToolsTests
+//
+//  Created by Matthew York on 3/22/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
