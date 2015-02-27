7ede0debfe4476902dbcd8f4e543802d7d6353c9
Piotr Zbyszyński <piotr@binarylabs.io>
2015-02-27 15:29:53 +0100

    - Added date parsing methods
    - Added dateWithString:formatString;
    - Added dateWithString:formatString:timeZone

--- DateTools/NSDate+DateTools.h
+++ DateTools/NSDate+DateTools.h
@@ -85,6 +85,9 @@ NSLocalizedStringFromTableInBundle(key, @"DateTools", [NSBundle bundleWithPath:[
 #pragma mark - Date Creating
 + (NSDate *)dateWithYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day;
 + (NSDate *)dateWithYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day hour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
++ (NSDate *)dateWithString:(NSString *)dateString formatString:(NSString *)formatString;
++ (NSDate *)dateWithString:(NSString *)dateString formatString:(NSString *)formatString timeZone:(NSTimeZone *)timeZone;
+
 
 #pragma mark - Date Editing
 #pragma mark Date By Adding
