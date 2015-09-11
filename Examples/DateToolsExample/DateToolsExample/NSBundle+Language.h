5066014f279a00f6b286153f48bbe87a750ff983
Yeung Yiu Hung <hkclex@gmail.com>
2015-09-11 12:08:14 +0800

    +Set language for date tools localization

---
+++ Examples/DateToolsExample/DateToolsExample/NSBundle+Language.h
@@ -0,0 +1,14 @@
+//
+//  NSBundle+Language.h
+//
+//
+//  Created by Yeung Yiu Hung on 11/9/15.
+//
+
+#import <Foundation/Foundation.h>
+#import "DTConstants.h"
+@interface NSBundle (Language)
+
++(void)setLanguage:(NSString*)language;
+
+@end
