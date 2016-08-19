2865d0dfc2b87a6189396a040d84d496d71e1fc3
Matt York <myork@cs.ua.edu>
2016-08-19 10:04:27 -0500

    - Created fresh example application on swift branch

---
+++ Examples/DateToolsExample/DateToolsExample/DateToolsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateToolsExample
+//
+//  Created by Matthew York on 8/19/16.
+//  Copyright © 2016 Matthew York. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
