9bc31dc13b656ccd355feef9ef1b209168827f1d
Greyson Wright <greysonmwright@gmail.com>
2016-08-23 00:16:06 -0500

    - added a few date building methods

--- DateTools/Date+DateTools.swift
+++ DateTools/Date+DateTools.swift
@@ -9,9 +9,40 @@
 import Foundation
 
 /**
- 
- */
+
+*/
 
 extension Date {
-    
+	static func date(with year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) -> Date? {
+		var dateComponents = DateComponents()
+		dateComponents.year = year
+		dateComponents.month = month
+		dateComponents.day = day
+		dateComponents.hour = hour
+		dateComponents.minute = minute
+		dateComponents.second = second
+		
+		let date = Calendar.current.date(from: dateComponents)
+		return date;
+		
+	}
+	
+	static func date(with year: Int, month: Int, day: Int) -> Date? {
+		return date(with: year, month: month, day: day, hour: 0, minute: 0, second: 0)
+	}
+	
+	static func date(with dateString: String, format: String, timeZone: TimeZone) -> Date? {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateStyle = .none;
+		dateFormatter.timeStyle = .none;
+		dateFormatter.timeZone = timeZone;
+		dateFormatter.dateFormat = format;
+		
+		let date = dateFormatter.date(from: dateString)
+		return date
+	}
+	
+	static func date(with dateString: String, format: String) -> Date? {
+		return date(with: dateString, format: format, timeZone: TimeZone.current)
+	}
 }
