f8cf568dd6579a8f528a2d61242da5d593446e05
Matt York <myork@cs.ua.edu>
2016-08-23 15:29:40 -0500

    - Moved a few things around for code clarity - Created format and time ago extension files for the date class

---
+++ DateTools/Date+Format.swift
@@ -0,0 +1,55 @@
+//
+//  Date+Format.swift
+//  DateToolsTests
+//
+//  Created by Matthew York on 8/23/16.
+//  Copyright © 2016 Matthew York. All rights reserved.
+//
+
+import UIKit
+
+extension Date {
+    //MARK: Formatted Date - Style
+    func format(with dateStyle: DateFormatter.Style, timeZone: TimeZone, locale: Locale) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeZone = timeZone
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: self)
+    }
+    
+    func format(with dateStyle: DateFormatter.Style, timeZone: TimeZone) -> String? {
+        return format(with: dateStyle, timeZone: timeZone, locale: Locale.autoupdatingCurrent)
+    }
+    
+    func format(with dateStyle: DateFormatter.Style, locale: Locale) -> String? {
+        return format(with: dateStyle, timeZone: TimeZone.autoupdatingCurrent, locale: locale)
+    }
+    
+    func format(with dateStyle: DateFormatter.Style) -> String? {
+        return format(with: dateStyle, timeZone: TimeZone.autoupdatingCurrent, locale: Locale.autoupdatingCurrent)
+    }
+    
+    //MARK: Formatted Date - String
+    func format(with dateFormat: String, timeZone: TimeZone, locale: Locale) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        dateFormatter.timeZone = timeZone
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: self)
+    }
+    
+    func format(with dateFormat: String, timeZone: TimeZone) -> String? {
+        return format(with: dateFormat, timeZone: timeZone, locale: Locale.autoupdatingCurrent)
+    }
+    
+    func format(with dateFormat: String, locale: Locale) -> String? {
+        return format(with: dateFormat, timeZone: TimeZone.autoupdatingCurrent, locale: locale)
+    }
+    
+    func format(with dateFormat: String) -> String? {
+        return format(with: dateFormat, timeZone: TimeZone.autoupdatingCurrent, locale: Locale.autoupdatingCurrent)
+    }
+}
---
+++ DateTools/Date+TimeAgo.swift
@@ -0,0 +1,13 @@
+//
+//  Date+TimeAgo.swift
+//  DateToolsTests
+//
+//  Created by Matthew York on 8/23/16.
+//  Copyright © 2016 Matthew York. All rights reserved.
+//
+
+import UIKit
+
+extension Date {
+    
+}
