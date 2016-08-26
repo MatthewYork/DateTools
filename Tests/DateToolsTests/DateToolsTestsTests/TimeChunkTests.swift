debadb6d2cb9674efe1385f0315fedaf504f8949
wgwebster <wgwebster@crimson.ua.edu>
2016-08-26 14:18:32 -0500

    - Added TimeChunk Tests

---
+++ Tests/DateToolsTests/DateToolsTestsTests/TimeChunkTests.swift
@@ -0,0 +1,26 @@
+//
+//  TimeChunkTests.swift
+//  DateToolsTests
+//
+//  Created by Grayson Webster on 8/26/16.
+//  Copyright © 2016 Matthew York. All rights reserved.
+//
+
+import XCTest
+@testable import DateToolsTests
+
+class TimeChunkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+}
