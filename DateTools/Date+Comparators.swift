fadd9ea949c30b95d60d99b5bba98ac1f35f433e
Greyson Wright <greysonmwright@gmail.com>
2016-08-29 10:46:22 -0500

    - moved date tools comparison functions and operators to the appropriate file extension

--- DateTools/Date+Comparators.swift
+++ DateTools/Date+Comparators.swift
@@ -9,5 +9,57 @@
 import Foundation
 
 extension Date {
-
+	
+	// MARK: - Addition / Subtractions
+	
+	func add(_ timeChunk: TimeChunk) -> Date {
+		return Date()
+	}
+	
+	func subtract(_ timeChunk: TimeChunk) -> Date {
+		return Date()
+	}
+	
+	// MARK: - Time Between
+	
+	func timeBetween(date: Date) -> TimeInterval {
+		return TimeInterval()
+	}
+	
+	
+	// MARK: - Comparisons
+	
+	func equals(_ date: Date) -> Bool {
+		return self.compare(date) == .orderedSame
+	}
+	
+	func greater(than date: Date) -> Bool {
+		return self.compare(date) == .orderedDescending
+	}
+	
+	func less(than date: Date) -> Bool {
+		return self.compare(date) == .orderedAscending
+	}
+	
+	// MARK: - Operator Overloads
+	
+	static func +(leftAddend: Date, rightAddend: TimeChunk) -> Date {
+		return leftAddend.add(rightAddend)
+	}
+	
+	static func -(minuend: Date, subtrahend: TimeChunk) -> Date {
+		return minuend.subtract(subtrahend)
+	}
+	
+	static func >(left: Date, right: Date) -> Bool {
+		return left.greater(than: right)
+	}
+	
+	static func <(left: Date, right: Date) -> Bool {
+		return left.less(than: right)
+	}
+	
+	static func ==(left: Date, right: Date) -> Bool {
+		return left.equals(right)
+	}
 }
