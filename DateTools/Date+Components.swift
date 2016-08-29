a0fe0afe67bcb4ed31bacdd1b952de65641357fc
Greyson Wright <greysonmwright@gmail.com>
2016-08-29 13:35:03 -0500

    - developed out component methods

--- DateTools/Date+Components.swift
+++ DateTools/Date+Components.swift
@@ -9,94 +9,139 @@
 import Foundation
 
 extension Date {
-
-    // MARK: - Components
-    
-    var era: Int {
-        return 0
-    }
-    
-    var year: Int {
-        return 0
-    }
-    
-    var month: Int {
-        return 0
-    }
-    
-    var week: Int {
-        return 0
-    }
-    
-    var day: Int {
-        return 0
-    }
-    
-    var hour: Int {
-        return 0
-    }
-    
-    var minute: Int {
-        return 0
-    }
-    
-    var second: Int {
-        return 0
-    }
-    
-    var weekday: Int {
-        return 0
-    }
-    
-    var weekdayOrdinal: Int {
-        return 0
-    }
-    
-    var quarter: Int {
-        return 0
-    }
-    
-    var weekOfMonth: Int {
-        return 0
-    }
-    
-    var weekOfYear: Int {
-        return 0
-    }
-    
-    var yearForWeekOfYear: Int {
-        return 0
-    }
-    
-    var daysInMonth: Int {
-        return 0
-    }
-    
-    var dayOfYear: Int {
-        return 0
-    }
-    
-    var daysInYear: Int {
-        return 0
-    }
-    
-    var isInLeapYear: Bool {
-        return false
-    }
-    
-    var isToday: Bool {
-        return false
-    }
-    
-    var isTomorrow: Bool {
-        return false
-    }
-    
-    var isYesterday: Bool {
-        return false
-    }
-    
-    var isWeekend: Bool {
-        return false
-    }
+	func component(_ component: Calendar.Component, from date: Date) -> Int {
+		let calendar = Calendar.autoupdatingCurrent
+		return calendar.component(component, from: date)
+	}
+	
+	func ordinality(of smaller: Calendar.Component, in larger: Calendar.Component, for date: Date) -> Int? {
+		let calendar = Calendar.autoupdatingCurrent
+		return calendar.ordinality(of: smaller, in: larger, for: date)
+	}
+	
+	func unit(of smaller: Calendar.Component, in larger: Calendar.Component, date: Date) -> Int? {
+		let calendar = Calendar.autoupdatingCurrent
+		let units = calendar.range(of: .day, in: .month, for: date)
+		return units?.count
+	}
+	
+	// MARK: - Components
+	
+	var era: Int {
+		return component(.era, from: self)
+	}
+	
+	var year: Int {
+		return component(.year, from: self)
+	}
+	
+	var month: Int {
+		return component(.month, from: self)
+	}
+	
+	var week: Int {
+		return component(.weekday, from: self)
+	}
+	
+	var day: Int {
+		return component(.day, from: self)
+	}
+	
+	var hour: Int {
+		return component(.hour, from: self)
+	}
+	
+	var minute: Int {
+		return component(.minute, from: self)
+	}
+	
+	var second: Int {
+		return component(.second, from: self)
+	}
+	
+	var weekday: Int {
+		return component(.weekday, from: self)
+	}
+	
+	var weekdayOrdinal: Int {
+		return component(.weekdayOrdinal, from: self)
+	}
+	
+	var quarter: Int {
+		return component(.quarter, from: self)
+	}
+	
+	var weekOfMonth: Int {
+		return component(.weekOfMonth, from: self)
+	}
+	
+	var weekOfYear: Int {
+		return component(.weekOfYear, from: self)
+	}
+	
+	var yearForWeekOfYear: Int {
+		return component(.yearForWeekOfYear, from: self)
+	}
+	
+	var dayOfYear: Int {
+		guard let dayOfYear = ordinality(of: .day, in: .year, for: self) else {
+			return 0
+		}
+		return dayOfYear
+	}
+	
+	var daysInMonth: Int {
+		guard let unit = unit(of: .day, in: .month, date: self) else {
+			return 0
+		}
+		return unit
+	}
+	
+	var daysInYear: Int {
+		guard let unit = unit(of: .day, in: .year, date: self) else {
+			return 0
+		}
+		return unit
+	}
+	
+	var isInLeapYear: Bool {
+		let yearComponent = component(.year, from: self)
+		
+		if yearComponent % 400 == 0 {
+			return true
+		}
+		if yearComponent % 100 == 0 {
+			return false
+		}
+		if yearComponent % 4 == 0 {
+			return true
+		}
+		return false
+	}
+	
+	var isToday: Bool {
+		let calendar = Calendar.autoupdatingCurrent
+		return calendar.isDateInToday(self)
+	}
+	
+	var isTomorrow: Bool {
+		if Date().add(1.days) == self {
+			return true
+		}
+		return false
+	}
+	
+	var isYesterday: Bool {
+		if Date().subtract(1.days) == self {
+			return true
+		}
+		return false
+	}
+	
+	var isWeekend: Bool {
+		if weekday == 6 || weekday == 7 {
+			return true
+		}
+		return false
+	}
 }
