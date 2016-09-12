56e0c7615b9a5459a8517c14a20062b276ccbcfc
wgwebster <wgwebster@crimson.ua.edu>
2016-09-12 15:42:23 -0500

    - Wrote tests for TimePeriodChain

--- Tests/DateToolsTests/DateToolsTestsTests/TimePeriodChainTests.swift
+++ Tests/DateToolsTests/DateToolsTestsTests/TimePeriodChainTests.swift
@@ -20,8 +20,8 @@ class TimePeriodChainTests : XCTestCase {
         self.formatter.dateFormat = "yyyy MM dd HH:mm:ss.SSS"
         //Create test TimePeriods that are 1 year long
         let firstPeriod = TimePeriod(beginning: self.formatter.date(from: "2014 11 05 18:15:12.000")!, end: self.formatter.date(from: "2015 11 05 18:15:12.000")!)
-        let secondPeriod: TimePeriod = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2016 11 05 18:15:12.000")!)
-        let thirdPeriod: TimePeriod = TimePeriod(beginning: self.formatter.date(from: "2016 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        let secondPeriod = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2016 11 05 18:15:12.000")!)
+        let thirdPeriod = TimePeriod(beginning: self.formatter.date(from: "2016 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
         //Add test periods
         self.controlChain.append(firstPeriod)
         self.controlChain.append(secondPeriod)
@@ -36,20 +36,39 @@ class TimePeriodChainTests : XCTestCase {
     
     // MARK: - Chain Manipulation
     
-    func testAppend() {
-        
+    func testAppendPeriod() {
+        let testPeriod = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        controlChain.append(testPeriod)
+        XCTAssertTrue(controlChain.count == 4)
+        XCTAssertTrue(controlChain[3] as! TimePeriod == testPeriod)
+    }
+    
+    func testAppendGroup() {
+        let testCollection = TimePeriodCollection()
+        let testPeriod1 = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        let testPeriod2 = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2018 11 05 18:15:12.000")!)
+        let testPeriod3 = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2019 11 05 18:15:12.000")!)
+        testCollection.append(testPeriod1)
+        testCollection.append(testPeriod2)
+        testCollection.append(testPeriod3)
+        controlChain.append(testCollection)
+        XCTAssertTrue(controlChain.count == 6)
     }
     
     func testInsert() {
-        
+        let testPeriod = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        controlChain.insert(testPeriod, at: 2)
+        XCTAssertTrue(controlChain[2] as! TimePeriod == testPeriod)
     }
     
     func testRemove() {
-        
+        controlChain.remove(at: 1)
+        XCTAssertTrue(controlChain.count == 1)
     }
     
     func testRemoveAll() {
-        
+        controlChain.removeAll()
+        XCTAssertTrue(controlChain.count == 0)
     }
     
     func testMap() {
@@ -65,7 +84,10 @@ class TimePeriodChainTests : XCTestCase {
     }
     
     func testPop() {
-        
+        let testPeriod: TimePeriod = controlChain.pop() as! TimePeriod
+        let comparePeriod = TimePeriod(beginning: self.formatter.date(from: "2016 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        XCTAssertTrue(controlChain.count == 2)
+        XCTAssertTrue(testPeriod == comparePeriod)
     }
     
     
@@ -83,7 +105,14 @@ class TimePeriodChainTests : XCTestCase {
     // MARK: - Chain Relationship
     
     func testEquals() {
-        
+        let testChain = TimePeriodChain()
+        let testPeriod1 = TimePeriod(beginning: self.formatter.date(from: "2014 11 05 18:15:12.000")!, end: self.formatter.date(from: "2015 11 05 18:15:12.000")!)
+        let testPeriod2 = TimePeriod(beginning: self.formatter.date(from: "2015 11 05 18:15:12.000")!, end: self.formatter.date(from: "2016 11 05 18:15:12.000")!)
+        let testPeriod3 = TimePeriod(beginning: self.formatter.date(from: "2016 11 05 18:15:12.000")!, end: self.formatter.date(from: "2017 11 05 18:15:12.000")!)
+        testChain.append(testPeriod1)
+        testChain.append(testPeriod2)
+        testChain.append(testPeriod3)
+        XCTAssertTrue(controlChain.equals(chain: testChain))
     }
     
     
