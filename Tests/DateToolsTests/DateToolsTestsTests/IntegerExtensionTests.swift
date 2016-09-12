f26da7e3203b028e258a99421de65226b85d37bd
wgwebster <wgwebster@crimson.ua.edu>
2016-09-12 09:37:26 -0500

    - IntegerExtensionTests written

--- Tests/DateToolsTests/DateToolsTestsTests/IntegerExtensionTests.swift
+++ Tests/DateToolsTests/DateToolsTestsTests/IntegerExtensionTests.swift
@@ -7,12 +7,14 @@
 //
 
 import XCTest
+@testable import DateToolsTests
 
 class IntegerExtensionTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -20,16 +22,53 @@ class IntegerExtensionTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSecondsChunk() {
+        let testChunk = 1.seconds
+        let controlChunk = TimeChunk(seconds: 1, minutes: 0, hours: 0, days: 0, weeks: 0, months: 0, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.seconds)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMinutesChunk() {
+        let testChunk = 1.minutes
+        let controlChunk = TimeChunk(seconds: 0, minutes: 1, hours: 0, days: 0, weeks: 0, months: 0, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.minutes)
+    }
+    
+    func testHoursChunk() {
+        let testChunk = 1.hours
+        let controlChunk = TimeChunk(seconds: 0, minutes: 0, hours: 1, days: 0, weeks: 0, months: 0, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.hours)
+    }
+    
+    func testDaysChunk() {
+        let testChunk = 1.days
+        let controlChunk = TimeChunk(seconds: 0, minutes: 0, hours: 0, days: 1, weeks: 0, months: 0, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.days)
+    }
+    
+    func testWeeksChunk() {
+        let testChunk = 1.weeks
+        let controlChunk = TimeChunk(seconds: 0, minutes: 0, hours: 0, days: 0, weeks: 1, months: 0, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.weeks)
+    }
+    
+    func testMonthsChunk() {
+        let testChunk = 1.months
+        let controlChunk = TimeChunk(seconds: 0, minutes: 0, hours: 0, days: 0, weeks: 0, months: 1, years: 0)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.months)
+    }
+    
+    func testYearsChunk() {
+        let testChunk = 1.years
+        let controlChunk = TimeChunk(seconds: 0, minutes: 0, hours: 0, days: 0, weeks: 0, months: 0, years: 1)
+        XCTAssertTrue(testChunk == controlChunk)
+        XCTAssertFalse(testChunk == 4.years)
     }
     
 }
