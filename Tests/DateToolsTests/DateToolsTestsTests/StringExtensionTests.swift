4c0df78e54b9b1b58c990e17c66beadd62d6e802
wgwebster <wgwebster@crimson.ua.edu>
2016-09-12 09:37:48 -0500

    - StringExtensionTest written

--- Tests/DateToolsTests/DateToolsTestsTests/StringExtensionTests.swift
+++ Tests/DateToolsTests/DateToolsTestsTests/StringExtensionTests.swift
@@ -7,12 +7,14 @@
 //
 
 import XCTest
+@testable import DateToolsTests
 
 class StringExtensionTests: XCTestCase {
     
+    let controlDate = Date()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
@@ -20,16 +22,9 @@ class StringExtensionTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testStringExtension() {
+        let dateString = String(controlDate)
+        XCTAssertTrue(dateString == String(Date()))
     }
     
 }
