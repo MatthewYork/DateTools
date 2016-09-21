adfa7913cb62ca24cc6a3c7884715a587917b106
wgwebster <wgwebster@crimson.ua.edu>
2016-09-21 15:17:36 -0500

    - Finished Format tests

--- Tests/DateToolsTests/DateToolsTestsTests/DateFormatExtensionTests.swift
+++ Tests/DateToolsTests/DateToolsTestsTests/DateFormatExtensionTests.swift
@@ -7,12 +7,18 @@
 //
 
 import XCTest
+@testable import DateToolsTests
 
 class DateFormatTests: XCTestCase {
+    
+    var formatter = DateFormatter()
+    var controlDate = Date()
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.formatter.dateFormat = "yyyy MM dd HH:mm:ss.SSS"
+        controlDate = self.formatter.date(from: "2011 12 25 4:30:30.000")!
     }
     
     override func tearDown() {
@@ -20,16 +26,58 @@ class DateFormatTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // MARK: - Formatted Date - Style
+    
+    func testFormatStyle() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: DateFormatter.Style.long, timeZone: TimeZone.autoupdatingCurrent, locale: Locale.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "December 25, 2011")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testFormatStyleWithoutTimezone() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: DateFormatter.Style.medium, locale: Locale.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "Dec 25, 2011")
+    }
+    
+    func testFormatStyleWithoutLocale() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: DateFormatter.Style.short, timeZone: TimeZone.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "12/25/11")
+    }
+    
+    func testFormatStyleAlone() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: DateFormatter.Style.full)
+        XCTAssertTrue(formatString == "Sunday, December 25, 2011")
+    }
+    
+    
+    // MARK: - Formatted Date - String
+    
+    func testFormatString() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: "yyyy MM dd", timeZone: TimeZone.autoupdatingCurrent, locale: Locale.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "2011 12 25")
+    }
+    
+    func testFormatStringWithoutTimezone() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: "dd-MM-yyyy", locale: Locale.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "25-12-2011")
+    }
+    
+    func testFormatStringWithoutLocale() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: "MM/dd/yy", timeZone: TimeZone.autoupdatingCurrent)
+        XCTAssertTrue(formatString == "12/25/11")
+    }
+    
+    func testFormatStringAlone() {
+        let testDate = Date(dateString: "2011 12 25 4:30:30.000", format: "yyyy MM dd HH:mm:ss.SSS")
+        let formatString = testDate.format(with: "EEE, MMMM dd, yyyy")
+        XCTAssertTrue(formatString == "Sun, December 25, 2011")
     }
     
 }
