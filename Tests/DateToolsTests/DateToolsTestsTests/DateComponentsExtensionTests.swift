3720b89db6f7f43a53110c5280fde54accd466a8
wgwebster <wgwebster@crimson.ua.edu>
2016-10-07 15:36:34 -0500

    - Added a test to Components tests

--- Tests/DateToolsTests/DateToolsTestsTests/DateComponentsExtensionTests.swift
+++ Tests/DateToolsTests/DateToolsTestsTests/DateComponentsExtensionTests.swift
@@ -39,6 +39,7 @@ class DateComponentsTests: XCTestCase {
     func testUnit() {
         let testDate = self.formatter.date(from: "2014 05 26 18:15:12.000")!
         let testDate2 = self.formatter.date(from: "2012 05 26 18:15:12.000")!
+        let testDate3 = self.formatter.date(from: "2012 02 26 18:15:12.000")!
         XCTAssertTrue(testDate.unit(of: .month, in: .year) == 12)
         XCTAssertTrue(testDate.unit(of: .day, in: .month) == 31)
         XCTAssertTrue(testDate.unit(of: .day, in: .year) == 365)
@@ -53,6 +54,7 @@ class DateComponentsTests: XCTestCase {
         XCTAssertTrue(testDate2.unit(of: .day, in: .year) == 366)
         XCTAssertTrue(testDate2.unit(of: .hour, in: .year) == 366 * 24)
         XCTAssertTrue(testDate2.unit(of: .second, in: .year) == 60 * 60 * 24 * 366)
+        XCTAssertTrue(testDate3.unit(of: .day, in: .month) == 29)
         // Equality test
         XCTAssertTrue(testDate.unit(of: .day, in: .month)! == controlDate.unit(of: .day, in: .month)! + 1)
     }
