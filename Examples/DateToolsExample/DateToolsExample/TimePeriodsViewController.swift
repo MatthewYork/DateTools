9fbc0954b296181391acbc5723c8a2854ca78bdb
wgwebster <wgwebster@crimson.ua.edu>
2016-10-26 17:03:51 -0500

    - Started TimePeriods example

--- Examples/DateToolsExample/DateToolsExample/TimePeriodsViewController.swift
+++ Examples/DateToolsExample/DateToolsExample/TimePeriodsViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class TimePeriodsViewController: UIViewController {
 
+    @IBOutlet weak var periodA: UIView!
+    @IBOutlet weak var periodB: UIView!
+    @IBOutlet weak var periodC: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +25,14 @@ class TimePeriodsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            let xPoint = max((view.frame.width / 2) + 10, min(view.center.x + translation.x, self.view.center.x + (self.view.frame.width / 2) - (view.frame.width / 2) - 10))
+            let yPoint = view.center.y
+            view.center = CGPoint(x: xPoint, y: yPoint)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
 }
-
