ded66f702376c1d660721ee55fb41c6a31822403
Andrew Breckenridge <asbreckenridge@me.com>
2016-11-14 15:31:34 -0800

    delete migrated folders

--- Tests/DateToolsTests/DateToolsTestsTests/TimePeriodCollection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TimePeriodCollection.swift
-//  DateToolsTests
-//
-//  Created by Grayson Webster on 8/19/16.
-//  Copyright © 2016 Matthew York. All rights reserved.
-//
-
-import Foundation
