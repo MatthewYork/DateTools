ff9235339b1102777d855b3e466736cb31a9a12a
Mani Ramezan <mani.ramezan@gmail.com>
2020-02-09 19:07:22 -0500

    Add SwiftPM support

--- Package.swift
+++ Package.swift
@@ -1,9 +1,30 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"])
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(
+            name: "DateToolsSwift",
+            path: "DateToolsSwift/DateTools",
+            exclude: [
+                "Examples",
+                "DateToolsSwift/Examples"
+            ]
+        ),
+        .testTarget(
+            name: "DateToolsSwiftTests",
+            dependencies: ["DateToolsSwift"],
+            path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests"
+        )
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
