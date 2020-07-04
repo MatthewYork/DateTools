7f61fb6f1cd6c9925244395a6129b175bb21080d
Mani Ramezan <mani.ramezan@gmail.com>
2020-07-04 19:23:30 -0400

    Fix failing tests

--- DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests/TimeAgoTests.swift
+++ DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests/TimeAgoTests.swift
@@ -12,7 +12,7 @@ import XCTest
 
 class TimeAgoTests : XCTestCase {
     
-    var formatter: DateFormatter = {
+    let formatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy MM dd HH:mm:ss.SSS"
         return formatter
@@ -39,51 +39,51 @@ class TimeAgoTests : XCTestCase {
     
     func testBasicLongTimeAgo() {
         let now: String = self.date0.timeAgo(since: self.date0)
-        XCTAssert(now.isEmpty, "'Now' is empty.")
+        XCTAssertFalse(now.isEmpty, "'Now' is empty.")
         let ago: String = self.date1.timeAgo(since: self.date0)
-        XCTAssert(ago.isEmpty, "Ago is empty.")
+        XCTAssertFalse(ago.isEmpty, "Ago is empty.")
     }
     
     func testLongTimeAgo2Days() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 05 18:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("2 days ago"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("2 days ago"))
     }
     
     func testLongTimeAgo1DayAndHalf() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 9:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("Yesterday"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("Yesterday"))
     }
     
     func testLongTimeAgoExactlyYesterday() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 18:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("Yesterday"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("Yesterday"))
     }
     
     func testLongTimeAgoLessThan24hoursButYesterday() throws {
-        self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 20:15:12.000"))
-        self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
+        self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 00:15:12.000"))
+        self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 22:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("22 hours ago"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("22 hours ago"))
     }
     
     func testLongTimeAgoLessThan24hoursSameDay() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 10:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("8 hours ago"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("8 hours ago"))
     }
     
     func testLongTimeAgoBetween24And48Hours() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 10:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 08 18:15:12.000"))
         let ago: String = self.date0.timeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("Yesterday"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("Yesterday"))
     }
     
     func testBasicShortTimeAgo() {
@@ -97,58 +97,66 @@ class TimeAgoTests : XCTestCase {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 05 18:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("2d"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("2d"))
     }
     
     func testShortTimeAgo1DayAndHalf() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 9:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("1d"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("1d"))
     }
     
     func testShortTimeAgoExactlyYesterday() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 18:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("1d"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("1d"))
     }
     
     func testShortTimeAgoLessThan24hoursButYesterday() throws {
-        self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 06 20:15:12.000"))
-        self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
+        self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 00:15:12.000"))
+        self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 22:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("22h"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("22h"))
     }
     
     func testShortTimeAgoLessThan24hoursSameDay() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 10:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 18:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("8h"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("8h"))
     }
     
     func testShortTimeAgoBetween24And48Hours() throws {
         self.date0 = try XCTUnwrap(self.formatter.date(from: "2014 11 07 10:15:12.000"))
         self.date1 = try XCTUnwrap(self.formatter.date(from: "2014 11 08 18:15:12.000"))
         let ago: String = self.date0.shortTimeAgo(since: self.date1)
-        XCTAssertEqual(ago, DateToolsLocalizedStrings("1d"))
+        XCTAssertEqual(ago, try DateToolsLocalizedStrings("1d"))
     }
     
     func testLongTimeAgoLocalizationsAccessible() throws {
-        let en_local: String = "Yesterday"
-        let ja_local: String = "昨日"
-        let key: String = en_local
-        let ja_result: String = DateToolsLocalizedStrings(key)
+        let en_local = "Yesterday"
+        let ja_local = "昨日"
+        let key = en_local
+        let ja_result = try DateToolsLocalizedStrings(key, for: .japanese)
         XCTAssertEqual(ja_local, ja_result, "Could not access localizations.")
     }
 }
 
 extension XCTestCase {
-    func DateToolsLocalizedStrings(_ key: String) -> String {
+    enum LanguageIdentifier: String {
+        case english = "en"
+        case japanese = "ja"
+    }
+    func DateToolsLocalizedStrings(_ key: String, for language: LanguageIdentifier = .english) throws -> String {
+        let dateToolsBundlePath = try XCTUnwrap(Bundle.module.path(forResource: "DateTools", ofType: "bundle"))
+        let dateToolsBundle = try XCTUnwrap(Bundle(path: dateToolsBundlePath))
+        let path = try XCTUnwrap(dateToolsBundle.path(forResource: language.rawValue, ofType: "lproj"))
+        let languageBundle = try XCTUnwrap(Bundle(path: path))
         return NSLocalizedString(key,
                                  tableName: "DateTools",
-                                 bundle: Bundle.dateToolsBundle(),
+                                 bundle: languageBundle,
                                  value: "",
                                  comment: "")
     }
