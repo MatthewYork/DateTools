318bbc634a994278f3b5366e6950831e56b7bf43
Serhii Yuryk <serhii.yuryk@wikrgroup.com>
2021-01-15 18:55:14 +0200

    Package file updated to Swift 5.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,56 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "DateToolsSwift",
+    name: "DateTools",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"]),
+        .library(
+            name: "DateToolsObjC",
+            targets: ["DateToolsObjC"]),
+    ],
+    dependencies: [],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(
+            name: "DateToolsSwift",
+            dependencies: [],
+            path: "DateToolsSwift/DateTools",
+            exclude: [
+                "DateTools",
+                "Examples",
+                "DateToolsSwift/Tests",
+                "DateToolsSwift/Examples",
+            ]
+        ),
+        .testTarget(
+            name: "DateToolsSwiftTests",
+            dependencies: ["DateToolsSwift"],
+            path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests",
+            exclude: []
+        ),
+        .target(
+            name: "DateToolsObjC",
+            dependencies: [],
+            path: "DateTools/DateTools",
+            exclude: [
+                "DateToolsSwift",
+                "Examples",
+                "DateTools/Tests",
+                "DateTools/Examples",
+            ]
+        ),
+        .testTarget(
+            name: "DateToolsObjCTests",
+            dependencies: ["DateToolsObjC"],
+            path: "DateTools/Tests/DateToolsTests/DateToolsTestsTests",
+            exclude: []
+        ),
+
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
