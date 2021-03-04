39765c2ba392b0a7928d715b4c91197abdca71cc
Jim Boyd <James.Boyd@ChurchofJesusChrist.org>
2021-03-04 15:42:26 -0700

    Update to swift PM 5.3

--- Package.swift
+++ Package.swift
@@ -1,9 +1,30 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_13),
+    ],
+    products: [
+        .library(
+            name: "DateToolsSwift", targets: ["DateToolsSwift"]),
+    ],
+    dependencies: [
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(name: "DateToolsSwift",
+                path: "DateToolsSwift/DateTools",
+                exclude: ["Tests", "Examples", "doc_gen.sh", "DateTools.bundle"]),
+//        .testTarget(
+//            name: "DateToolsTests",
+//            dependencies: ["DateToolsSwift"],
+//            path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests",
+//            exclude: ["DataTools", "Examples", "DateToolsTests", "doc_gen.sh", "Info.plist"]),
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+    
